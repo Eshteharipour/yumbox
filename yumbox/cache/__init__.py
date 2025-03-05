e9509182bc0d6062bf6d3801b3bca3e76919a0ae
ShayanEs <shayaneshteharipour@gmail.com>
2025-03-06 01:53:35 +0330

    feature update
    adding safe file op functions, made func kwarg have higher priority than class attributes on retry, added last_str_offset function

--- yumbox/cache/__init__.py
+++ yumbox/cache/__init__.py
@@ -1,6 +1,8 @@
 import functools
 import os
 import pickle
+import shutil
+import tempfile
 from collections.abc import Callable
 from time import sleep
 from typing import Iterable, Optional
@@ -9,10 +11,40 @@ import faiss
 import numpy as np
 from safetensors.numpy import load_file, save_file
 
-from yumbox.config import BFG, redir_print
+from yumbox.config import BFG
 
-# TODO: To prevent corruption on power outage,
-# save to temp location then move to target.
+
+def safe_save(file_path: str, save_func: Callable, *save_func_args, **save_func_kwargs):
+    temp_dir = os.path.dirname(file_path)
+    file_ext = os.path.splitext(file_path)[1] or None
+
+    with tempfile.NamedTemporaryFile(
+        delete=False, dir=temp_dir, suffix=file_ext
+    ) as tmp_file:
+        save_func(tmp_file.name, *save_func_args, **save_func_kwargs)
+
+    shutil.move(tmp_file.name, file_path)
+
+
+def safe_load():
+    pass
+
+
+def safe_openw(file_path: str, *save_func_args, **save_func_kwargs):
+    temp_dir = os.path.dirname(file_path)
+    file_ext = os.path.splitext(file_path)[1] or None
+
+    with tempfile.NamedTemporaryFile(
+        delete=False, dir=temp_dir, suffix=file_ext
+    ) as tmp_file:
+        with open(tmp_file.name) as fd:
+            fd.write(*save_func_args, **save_func_kwargs)
+
+    shutil.move(tmp_file.name, file_path)
+
+
+def safe_openr():
+    pass
 
 
 def cache(func):
@@ -248,7 +280,7 @@ class Error400(Exception):
     pass
 
 
-def retry(max_tries=5, wait=3, validator: Optional[Callable] = None):
+def retry(max_tries=None, wait=None, validator: Optional[Callable] = None):
     """Args max_tries and wait defined as class attributes have higher precedence."""
 
     def decorator(func):
@@ -257,8 +289,8 @@ def retry(max_tries=5, wait=3, validator: Optional[Callable] = None):
             logger = BFG["logger"]
 
             self = args[0] if len(args) else None
-            tries = coalesce(getattr(self, "max_tries", None), max_tries)
-            delay = coalesce(getattr(self, "wait", None), wait)
+            tries = coalesce(max_tries, getattr(self, "max_tries", None), 5)
+            delay = coalesce(wait, getattr(self, "wait", None), 3)
             success = False
             exception = None
             for retry in range(0, tries):
@@ -271,6 +303,8 @@ def retry(max_tries=5, wait=3, validator: Optional[Callable] = None):
                 except Error400 as e:
                     exception = e
                     logger.error(f"Exception {e} occured.")
+                    logger.error(f"Args: {args}")
+                    logger.error(f"KWArgs: {kwargs}")
                     break
                 except Exception as e:
                     exception = e
@@ -281,6 +315,8 @@ def retry(max_tries=5, wait=3, validator: Optional[Callable] = None):
                             f"Exception {e} occured, retrying {retry+1}/{tries}"
                         )
                         logger.warning(traceback.format_exc())
+                        logger.warning(f"Args: {args}")
+                        logger.warning(f"KWArgs: {kwargs}")
                         sleep(delay)
                     continue
             if success == True:
@@ -373,6 +409,73 @@ def last_offset(func):
             logger.info(f"Saved cache!")
 
         return output
+
+    return wrapper
+
+
+def last_str_offset(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        cache_dir = BFG["cache_dir"]
+        offset_cache_dir = cache_dir
+        logger = BFG["logger"]
+
+        func_kwargs = []
+        for k, v in kwargs.items():
+            if k.endswith("_cached"):
+                func_kwargs.append(f"{k}-{v}")
+        func_kwargs = " ".join(func_kwargs)
+
+        cache_func_name = func.__name__
+        cache_file = ""
+        if cache_dir:
+            cache_file = os.path.join(cache_dir, cache_func_name)
+            cache_file += ".pkl"
+        if cache_dir and os.path.isfile(cache_file):
+            logger.info(f"Loading cache for {cache_func_name} from {cache_dir}")
+            with open(cache_file, "rb") as fd:
+                output = pickle.load(fd)
+        else:
+            output = None
+
+        if func_kwargs:
+            offset_func_name = (
+                func.__name__ + "_" + func_kwargs + "_" + last_offset.__name__
+            )
+        else:
+            offset_func_name = func.__name__ + "_" + last_offset.__name__
+        offset_cache_file = ""
+        if offset_cache_dir:
+            offset_cache_file = os.path.join(offset_cache_dir, offset_func_name)
+            offset_cache_file += ".txt"
+
+        if offset_cache_dir and os.path.isfile(offset_cache_file):
+            logger.info(
+                f"Loading offset for {offset_func_name} from {offset_cache_dir}"
+            )
+            with open(offset_cache_file, "r") as fd:
+                offset = str(fd.readline().strip())
+        else:
+            offset = None
+
+        if output:
+            output, offset = func(*args, **kwargs, offset=offset, cached_output=output)
+        else:
+            output, offset = func(*args, **kwargs, offset=offset)
+
+        if offset_cache_dir and offset is not None:
+            logger.info(f"Saving offset for {offset_func_name} to {offset_cache_dir}")
+            with open(offset_cache_file, "w") as fd:
+                fd.write(str(offset))
+            logger.info(f"Saved offset!")
+
+        if cache_dir:
+            logger.info(f"Saving cache for {cache_func_name} to {cache_dir}")
+            with open(cache_file, "wb") as fd:
+                pickle.dump(output, fd)
+            logger.info(f"Saved cache!")
+
+        return output, offset
 
     return wrapper
 
