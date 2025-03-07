6c085be21a918cd883f31161643b4cddb0bdc4e9
ShayanEs <shayaneshteharipour@gmail.com>
2025-03-07 17:21:27 +0330

    feat: random_seed

--- yumbox/__init__.py
+++ yumbox/__init__.py
@@ -1,1 +1,20 @@
 from . import cache, config, data, nlp
+
+
+def random_seed(rank, seed=362):
+    import random
+
+    import numpy as np
+    import torch
+
+    torch.backends.cudnn.deterministic = True
+
+    torch.manual_seed(seed + rank)
+    torch.cuda.manual_seed_all(seed + rank)
+
+    np.random.seed(seed + rank)
+
+    random.seed(seed + rank)
+
+    rng = np.random.default_rng(seed + rank)
+    return rng
