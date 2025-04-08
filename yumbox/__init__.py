724e1309f15e6119bb39d3214f3557f000374227
ShayanEs <shayaneshteharipour@gmail.com>
2025-04-09 00:30:32 +0330

    random seed update

--- yumbox/__init__.py
+++ yumbox/__init__.py
@@ -1,13 +1,21 @@
+from typing import Literal
+
 from . import cache, config, data, factory, metrics, nlp, vectors
 
+RNG = {}
 
-def random_seed(rank, seed=362):
+
+def random_seed(rank, seed=362, generator: Literal["numpy", "torch", "int"] = "numpy"):
     import random
 
     import numpy as np
     import torch
 
+    # seed = torch.initial_seed()
+
     torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.benchmark = False  # across multiple executions
+    # torch.use_deterministic_algorithms(True)  # impacts performance
 
     torch.manual_seed(seed + rank)
     torch.cuda.manual_seed_all(seed + rank)
@@ -16,5 +24,28 @@ def random_seed(rank, seed=362):
 
     random.seed(seed + rank)
 
-    rng = np.random.default_rng(seed + rank)
+    if generator == "numpy":
+        rng = np.random.default_rng(seed + rank)
+    elif generator == "torch":
+        rng = torch.Generator()
+        rng.manual_seed(seed + rank)
+    else:
+        rng = seed + rank
+
+    RNG["rng"] = rng
+
     return rng
+
+
+def worker_seed(worker_id):
+    import torch
+
+    rank = torch.initial_seed() % 2**32
+    random_seed(rank)
+
+
+def get_dataset_batch_seed(
+    dataset_length: int, rows_trained: int, base_seed: int = 362
+) -> int:
+    iterations_completed = rows_trained // dataset_length
+    return base_seed + iterations_completed
