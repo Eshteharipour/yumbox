9327f664fb7bfd77a5780d928d5d80d79740fbb5
ShayanEs <shayaneshteharipour@gmail.com>
2025-05-01 10:19:30 +0330

    refactor

--- yumbox/metrics/__init__.py
+++ yumbox/metrics/__init__.py
@@ -1,6 +1,3 @@
-def jaccard_similarity(text1: str, text2: str) -> float:
-    set1 = set(text1.lower().split())
-    set2 = set(text2.lower().split())
-    intersection = len(set1 & set2)
-    union = len(set1 | set2)
-    return intersection / union if union > 0 else 0
+def jaccard_similarity(str1, str2):
+    set1, set2 = set(str1.lower().split()), set(str2.lower().split())
+    return len(set1 & set2) / len(set1 | set2)
