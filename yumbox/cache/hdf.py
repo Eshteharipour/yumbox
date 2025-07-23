70ca6fe9d53390393f80739333c05e29e1d6184d
ShayanEs <shayaneshteharipour@gmail.com>
2025-07-23 10:27:57 +0330

    fixed and updated hd5 cache

--- yumbox/cache/hdf.py
+++ yumbox/cache/hdf.py
@@ -16,34 +16,34 @@ def hd5_cache(func):
         func_name = func.__name__
         cache_file = os.path.join(cache_dir, f"{func_name}.h5") if cache_dir else None
 
-        keys = kwargs.get("keys", None)
-        features = kwargs.get("cache", {})
+        cache = {}
+        keys = kwargs.get("features", {}).keys()  # TODO: refactor features
 
         if cache_file and os.path.isfile(cache_file):
             logger.info(f"Loading cache for {func_name} from {cache_file}")
             try:
                 with h5py.File(cache_file, "r") as f:
                     # If keys are specified, load only those
-                    if keys is not None:
-                        for key in keys:
-                            if key in f:
-                                features[key] = np.array(f[key])
-                    # Otherwise, load all keys
-                    else:
-                        for key in f.keys():
-                            features[key] = np.array(f[key])
+                    for key in keys:
+                        if key in f:
+                            cache[key] = np.array(f[key])
+                    # load all keys:
+                    # for key in f.keys():
+                    #     cache[key] = np.array(f[key])
                 logger.info(f"Loaded cache for {func_name} from {cache_file}")
             except Exception as e:
                 logger.error(f"Failed to load cache: {e}")
-                features = {}
+                cache = {}
 
-        result = func(*args, **kwargs, cache=features, cache_file=cache_file)
+        result = func(*args, **kwargs, cache=cache, cache_file=cache_file)
 
         if cache_file:
             logger.info(f"Saving cache for {func_name} to {cache_file}")
             try:
-                with h5py.File(cache_file, "w") as f:
+                with h5py.File(cache_file, "a") as f:  # 'a': preserve previous content
                     for key, value in result.items():
+                        if key in f:
+                            del f[key]  # overwrite cache
                         f.create_dataset(key, data=value, compression="gzip")
                 logger.info(f"Saved cache for {func_name} to {cache_file}")
             except Exception as e:
